3d8303ad9bd67ca205cc1f3708937a61b4e23fac
sA-mb <94666612+sA-mb@users.noreply.github.com>
2022-11-03 22:38:16 -0500

    Update pokemon.py
    data frame que cumple con el requerimiento de listar nombre habilidades y url

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,17 @@
+#data frame para cumplir con la solicitud de listar
+
+import requests as re
+import pandas as pd
+
+pokemon = pd.DataFrame(columns=["nombre","habilidad","url"])
+
+def primera_generacion():
+    for i in range(1,152):
+        response = re.get(f"https://pokeapi.co/api/v2/pokemon/{i}")
+        data = response.json()
+        lista_habilidades = []
+        for x in response.json()["abilities"]:
+            lista_habilidades.append(x["ability"]["name"] )
+        pokemon.loc[i] = [data["name"],', '.join(lista_habilidades),f"https://pokeapi.co/api/v2/pokemon/{i}"]
+    print(pokemon)
 

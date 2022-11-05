d0114a6d6016bb661a615a273cee435e03ecd093
Luiskingbear <luisreynoso.al@gmailcom>
2022-11-05 01:17:21 -0500

    segunda version de opciones 1,2,3

---
+++ opciones.py
@@ -0,0 +1,34 @@
+from TrabajoFinUnidad.ramaLuis import *
+print("¿En que puedo ayudarte hoy? "
+      "\n1)  Cargar base de datos"
+      "\n2)  Listar libros"
+      "\n3)  Agregar libros"
+      "\n4)  Eliminar libros"
+      "\n5)  Buscar libros (ISBN/título)"
+      "\n6)  Ordenar libros por orden alfabetico"
+      "\n7)  Buscar libros (autor/editorial/genero)"
+      "\n8)  Buscar libros por cantidad de autores"
+      "\n9)  Actualizar datos de libros"
+      "\n10) Guardar nueva base de datos");
+while True:
+    a = int(input("SI QUIERE SALIR INGRESE 0\nINGRESA NUMERO"));
+    if a==0:
+        break;
+    if a == 1:
+        getCrearCsv()
+
+    if a == 2:
+        milibro = Libro('./bbdd.csv')
+        milibro.getLeerRegistrosLibros();
+    if a==3:
+        milibro = Libro('./bbdd.csv')
+        milibro.setRegistrarLibros();
+
+    # if a == 4:
+    #
+    # if a == 5:
+    # if a == 6:
+    # if a == 7:
+    # if a == 8:
+    # if a == 9:
+    # if a == 10:
--- ramaLuis.py
+++ ramaLuis.py
@@ -1,1 +1,90 @@
-print("hola")
+import pandas as pd
+from csv import *
+
+
+
+def getCrearCsv():
+    with open('./bbdd.csv', 'w') as file:
+        header = ('ID', 'TITULO', 'GENERO', 'ISBN', 'EDITORIAL', 'AUTORES')
+        csv_writer = writer(file, lineterminator='\n')
+        data = [
+            [1, "Amazing Spider-Man", "comic", "0785123370", "Marvel", "John Michael Straczynski / Ron Garney"],
+            [2, "Fight Club", "novela", "0393355942", "W. W. Norton & Company", "Chuck Palahniuk"],
+            [3, "Trainspotting", "novela", "9780393314809", "W. W. Norton & Company", "Irvine Welsh"]
+        ]
+
+        csv_writer.writerow(header)
+        csv_writer.writerows(data)
+
+    with open('./bbdd.csv') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            print(row)
+
+
+
+class Libro():
+    def __init__(self,ruta):
+        self.__ruta = ruta#'./bbdd.csv' #"bbdd.csv"
+        self.__df = pd.read_csv(self.__ruta)
+
+
+
+    def getLeerRegistrosLibros(self):
+        #nrows=3
+        with open(self.__ruta) as file:
+            csv_reader = reader(file)
+            for row in csv_reader:
+                print(row)
+        #print(self.__df)
+
+    def setRegistrarLibros(self):
+        #,id,titulo,genero,ISBN,editorial,autor
+
+        ID = []
+        TITULO = []
+        GENERO = []
+        ISBN = []
+        EDITORIAL = []
+        AUTOR = []
+        while True:
+            nuevo_ID = input("INGRESE su ID: ")
+            ID.append(nuevo_ID)
+
+            nuevo_TITULO = input("INGRESE TITULO: ")
+            TITULO.append(nuevo_TITULO)
+
+            nuevo_GENERO = input("INGRESE su GENERO: ")
+            GENERO.append(nuevo_GENERO)
+
+            nuevo_ISBN = input("INGRESE ISBN: ")
+            ISBN.append(nuevo_ISBN)
+
+            nuevo_EDITORIAL = input("INGRESE su EDITORIAL: ")
+            EDITORIAL.append(nuevo_EDITORIAL)
+
+            nuevo_AUTOR = input("PARA MAS DE 2 AUTORES SEPARAR CON / \nINGRESE AUTOR: ")
+            AUTOR.append(nuevo_AUTOR)
+
+            # data = {
+            #     "ID": ID,
+            #     "TITULO": TITULO,
+            #     "GENERO": GENERO,
+            #     "ISBN": ISBN,
+            #     "EDITORIAL": EDITORIAL,
+            #     "AUTOR": AUTOR
+            # };
+
+            a = input("SALIR? Y/N: ")
+            if a == "Y":
+                print("ADIOS")
+                break;
+            print("CONTINUAMOS")
+
+        columnas = ["ID", "TITULO", "GENERO", "ISBN", "EDITORIAL", "AUTOR"]
+
+        df = pd.DataFrame(list(zip(ID, TITULO, GENERO, ISBN, EDITORIAL, AUTOR)), columns=columnas)
+
+
+        df.to_csv(self.__ruta, index=None, mode="a", header=False)
+

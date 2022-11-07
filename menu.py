32584db3bd67c4fcd06d2f3e07faf7e69ffa220e
Luiskingbear <luisreynoso.al@gmailcom>
2022-11-06 23:34:05 -0500

    Actualizacion clase menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,18 @@
 import pokemon_class as pkc
 
+def opciones():
+    print("Eliga una opcion:\n",
+        "1.- Listar pokemon por generacion\n",
+        "2.- Listar pokemon por forma\n",
+        "3.- Listar pokemon por habilidad\n",
+        "4.- Listar pokemon por habitat\n",
+        "5.- Listar pokemon por tipo\n")
 def Menu():
-    print("Eliga una opcion:\n", "op1\n", "op2\n", "op3\n", "op4\n", "op5\n")
+    #print("Eliga una opcion:\n", "op1\n", "op2\n", "op3\n", "op4\n", "op5\n")
+    opciones()
     opcion =  input('Eliga una opcion: ')
     while opcion not in ['1','2','3','4','5']:
+        opciones()
         opcion =  input('Eliga una opcion (1, 2, 3, 4 o 5): ')
 
     if opcion == '1':
